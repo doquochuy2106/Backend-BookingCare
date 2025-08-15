2888462d612eb2ec5b35b72cf5708f03b97bb866
unknown <huy.do02062004@gmail.com>
2025-08-16 03:17:04 +0700

    video 26 CRUD với Node.js và Sequelize - Sử Dụng ORM Để Hiển Thị Dữ liệu (Read) - Phần 2

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -27,4 +27,16 @@ let postCRUD = async (req, res) => {
     return res.send("Post CRUD")
 }
 
-module.exports = { getHomePage, getAbout, getCRUD, postCRUD }
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDservice.getAllUser()
+    console.log("------------------------")
+    console.log(data)
+    console.log("------------------------")
+
+
+    return res.render("displayCRUD.ejs", {
+        dataTable: data
+    })
+}
+
+module.exports = { getHomePage, getAbout, getCRUD, postCRUD, displayGetCRUD }
