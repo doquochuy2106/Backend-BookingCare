d1cb45ead0413543f1ae85b9c76880f35d18e3b8
unknown <huy.do02062004@gmail.com>
2025-09-26 18:19:08 +0700

    video 67 Hoàn Thiện Chứa Năng Bổ Sung Thông Tin Chi Tiết Cho Bác Sỹ | Redux-React Cho Người Mới Bắt Đầu

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -17,6 +17,36 @@ let getTopDoctorHome = async (req, res) => {
     }
 }
 
+let getAllDoctors = async (req, res) => {
+    try {
+        let response = await doctorService.getAllDoctors()
+        return res.status(200).json(response)
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+
+let postInforDoctor = async (req, res) => {
+    try {
+        console.log(req.body)
+        let response = await doctorService.saveDetailInforDoctor(req.body)
+
+
+        return res.status(200).json(response)
+
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+
 module.exports = {
-    getTopDoctorHome
+    getTopDoctorHome, getAllDoctors, postInforDoctor
 }
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -28,6 +28,53 @@ let getTopDoctorHome = (limitInput) => {
     })
 }
 
+let getAllDoctors = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let doctors = await db.User.findAll({
+                where: { roleId: 'R2' },
+                attributes: {
+                    exclude: ["password", 'image']
+                },
+            })
+            resolve({
+                errCode: 0,
+                data: doctors
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let saveDetailInforDoctor = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.doctorId || !data.contentHTML || !data.contentMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing require parameter"
+                })
+            }
+            else {
+                await db.Markdown.create({
+                    contentHTML: data.contentHTML,
+                    contentMarkdown: data.contentMarkdown,
+                    description: data.description,
+                    doctorId: data.doctorId
+                })
+                resolve({
+                    errCode: 0,
+                    errMessage: "Save Infor Doctor Success"
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+
+}
+
 module.exports = {
-    getTopDoctorHome
+    getTopDoctorHome, getAllDoctors, saveDetailInforDoctor
 }
