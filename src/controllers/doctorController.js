70838a7d513eb6e778a5bfaba90c301090e8be6e
unknown <huy.do02062004@gmail.com>
2025-09-27 13:10:30 +0700

    video 68 Sequelize Relationship - Tìm Hiểu Quan Hệ Giữa Các Model & Database |React Cho Người Mới Bắt Đầu

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -47,6 +47,19 @@ let postInforDoctor = async (req, res) => {
     }
 }
 
+let getDetailDoctorById = async (req, res) => {
+    try {
+        let response = await doctorService.getDetailDoctorById(req.query.id)
+        return res.status(200).json(response)
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+
 module.exports = {
-    getTopDoctorHome, getAllDoctors, postInforDoctor
+    getTopDoctorHome, getAllDoctors, postInforDoctor, getDetailDoctorById
 }
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -75,6 +75,39 @@ let saveDetailInforDoctor = (data) => {
 
 }
 
+let getDetailDoctorById = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!id) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing require parameter"
+                })
+            }
+            else {
+                let data = await db.User.findOne({
+                    where: { id: id },
+                    attributes: { exclude: ['password', 'image'] },
+                    include: [
+                        { model: db.Markdown, attributes: ['description', 'contentHTML', 'contentMarkdown'] },
+                        { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+                    ],
+                    raw: true,
+                    nest: true
+                })
+                resolve({
+                    errCode: 0,
+                    data: data
+                })
+            }
+
+
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
-    getTopDoctorHome, getAllDoctors, saveDetailInforDoctor
+    getTopDoctorHome, getAllDoctors, saveDetailInforDoctor, getDetailDoctorById
 }
