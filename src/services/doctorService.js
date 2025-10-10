a035f3f51f45d811a7258cfba6fc89038b805f03
unknown <huy.do02062004@gmail.com>
2025-10-10 16:51:45 +0700

    video 80 Design Giao Diện Hiển Thị Thông Tin Khám Bệnh Của Bác Sĩ | Redux - React Cho Người Mới Bắt Đầu

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -149,6 +149,18 @@ let getDetailDoctorById = (id) => {
                     include: [
                         { model: db.Markdown, attributes: ['description', 'contentHTML', 'contentMarkdown'] },
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+
+                        {
+                            model: db.Doctor_Infor,
+                            attributes: {
+                                exclude: ['id', 'doctorId']
+                            },
+                            include: [
+                                { model: db.Allcode, as: 'priceTypeData', attributes: ['valueEn', 'valueVi'] },
+                                { model: db.Allcode, as: 'provinceTypeData', attributes: ['valueEn', 'valueVi'] },
+                                { model: db.Allcode, as: 'paymentTypeData', attributes: ['valueEn', 'valueVi'] },
+                            ]
+                        },
                     ],
                     raw: false,
                     nest: true
